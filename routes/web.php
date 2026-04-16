bea836a3477c5d4be2862533eacbc73a57269d64
Samir <120911119+samir2602@users.noreply.github.com>
2026-04-16 16:18:08 +0530

    Add controller and update route logic

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    $user = "Parth";
-    return view('hello', ['user' => $user]);
-});
+// Route::get('/hello', function () {
+//     $user = "Parth";
+//     return view('hello', ['user' => $user]);
+// });
+
+Route::get('/hello', [PageController::class, 'hello']);
