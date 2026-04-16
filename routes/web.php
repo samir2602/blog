c438407f705baec98c0abf6ae93bbcda5a953700
Samir <120911119+samir2602@users.noreply.github.com>
2026-04-16 16:12:09 +0530

    add hello world route and blade file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    $user = "Parth";
+    return view('hello', ['user' => $user]);
+});
